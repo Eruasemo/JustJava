78be60de8e2d52a2ff239e3857408d2ec5e939af
Manuel Balderas <tassadar46@gmail.com>
2024-09-03 21:13:34 -0600

    Commit de prueba

--- app/src/main/java/com/example/justjava/MainActivity.java
+++ app/src/main/java/com/example/justjava/MainActivity.java
@@ -1,6 +1,8 @@
 package com.example.justjava;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -14,5 +16,14 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void submitOrder(View view){
+        display(1);
+    }
+
+    public void display(int number){
+        TextView quantityTextView = (TextView) findViewById(R.id.quantity_text_view);
+        quantityTextView.setText(""+number);
     }
 }
